1e18a1482950bf22afb8372460e0d792a17b42ac
Black <dabould830@gmail.com>
2026-04-01 13:59:56 +0300

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
-    // تفعيل CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Cache-Control', 'no-store, max-age=0'); // إجبار أبل على عدم تخزين الرابط لتجنب أخطاء التثبيت
 
     if (req.method === 'OPTIONS') {
         res.status(200).end();
@@ -11,19 +11,13 @@ export default function handler(req, res) {
 
     const { ipa_url, bundle_id, version, app_name } = req.query;
 
-    if (!ipa_url) {
-        return res.status(400).send('Missing ipa_url parameter');
-    }
+    if (!ipa_url) return res.status(400).send('FATAL: Payload URL missing.');
 
-    // تأمين القيم الافتراضية، مع جعل "هذا التطبيق" هو الاسم الذي يظهر في رسالة iOS
-    const safeBundleId = bundle_id || 'com.otablack.app';
+    const safeBundleId = bundle_id || 'com.void.blackstore';
     const safeVersion = version || '1.0.0';
     const safeAppName = app_name || 'هذا التطبيق';
-    
-    // 🔥 تثبيت الصورة التي طلبتها لتظهر عند التثبيت 🔥
     const safeIconUrl = 'https://up6.cc/2026/04/177500568816331.png';
 
-    // بناء قالب XML الخاص بـ Apple OTA
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -42,12 +36,8 @@ export default function handler(req, res) {
                 <dict>
                     <key>kind</key>
                     <string>display-image</string>
-                    <key>url</key>
-                    <string><![CDATA[${safeIconUrl}]]></string>
-                </dict>
-                <dict>
-                    <key>kind</key>
-                    <string>full-size-image</string>
+                    <key>needs-shine</key>
+                    <false/>
                     <key>url</key>
                     <string><![CDATA[${safeIconUrl}]]></string>
                 </dict>
@@ -61,13 +51,13 @@ export default function handler(req, res) {
                 <key>kind</key>
                 <string>software</string>
                 <key>title</key>
-                <string><![CDATA[${safeAppName}]]></string> </dict>
+                <string><![CDATA[${safeAppName}]]></string>
+            </dict>
         </dict>
     </array>
 </dict>
 </plist>`;
 
-    // إرسال المحتوى كملف XML صالح
     res.setHeader('Content-Type', 'text/xml; charset=utf-8');
     res.status(200).send(xml);
 }
